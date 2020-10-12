8712800c036e775c0eb687ca053240e958403542
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2020-10-11 22:57:48 -0600

    add source

---
+++ include/ux/Separator.hpp
@@ -0,0 +1,15 @@
+#ifndef SAPI_UX_SEPARATOR_HPP
+#define SAPI_UX_SEPARATOR_HPP
+
+#include "Component.hpp"
+
+namespace ux {
+
+class Separator : public ComponentAccess<Separator> {
+public:
+  Separator(const var::String &name) : ComponentAccess(name) {}
+  void draw(const DrawingScaledAttributes &attributes);
+};
+} // namespace ux
+
+#endif // SEPARATOR_HPP
---
+++ src/sgfx/SvgFont.cpp
@@ -0,0 +1,15 @@
+/*! \file */ // Copyright 2011-2020 Tyler Gilbert and Stratify Labs, Inc; see LICENSE.md for rights.
+
+
+#include "sgfx/SvgFont.hpp"
+
+using namespace sgfx;
+
+SvgFont::SvgFont() {
+
+}
+
+SvgFont::~SvgFont() {
+
+}
+
